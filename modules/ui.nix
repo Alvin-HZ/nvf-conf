3dd7ea877bf6ed23c0d21790b3b28a7743b5f650
Alvin <153151121+Alvin-HZ@users.noreply.github.com>
2025-01-27 21:48:16 -0500

    add stuff

--- modules/ui.nix
+++ modules/ui.nix
@@ -1,17 +1,27 @@
 _: {
-  vim.ui = {
-    noice.enable = true;
-    colorizer.enable = true;
-    fastaction.enable = true;
-    breadcrumbs = {
-      lualine.winbar = {
+  vim = {
+    ui = {
+      noice.enable = true;
+      colorizer.enable = true;
+      fastaction.enable = true;
+      breadcrumbs = {
+        lualine.winbar = {
+          enable = true;
+          alwaysRender = true;
+        };
+        navbuddy = {
+          enable = true;
+        };
         enable = true;
-        alwaysRender = true;
       };
-      navbuddy = {
-        enable = true;
-      };
-      enable = true;
     };
+    keymaps = [
+      {
+        key = "<leader>lN";
+        mode = [""];
+        action = "<Cmd>Navbuddy<CR>";
+        desc = "Open Navbuddy";
+      }
+    ];
   };
 }
