28e706488923b9cc78acac701d82dc862c3451d0
Alvin <153151121+Alvin-HZ@users.noreply.github.com>
2025-01-27 22:05:52 -0500

    add language support

--- modules/languages.nix
+++ modules/languages.nix
@@ -7,7 +7,17 @@ _: {
       enableExtraDiagnostics = true;
 
       nix.enable = true;
+      markdown = {
+        enable = true;
+        format.enable = false;
+        extensions.render-markdown-nvim.enable = true;
+      };
       java.enable = true;
+      bash.enable = true;
+      rust = {
+        enable = true;
+        crates.enable = true;
+      };
     };
     lsp = {
       trouble.enable = true;
