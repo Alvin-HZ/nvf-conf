88d504bd1f931e54f8a2ca279247e0a4cbd9d6a1
Alvin <153151121+Alvin-HZ@users.noreply.github.com>
2025-01-27 22:50:54 -0500

    add obsidian conf

--- modules/notes.nix
+++ modules/notes.nix
@@ -1,5 +1,24 @@
 _: {
   vim.notes = {
     todo-comments.enable = true;
+    obsidian = {
+      enable = false;
+      setupOpts = {
+        completion = {
+          nvim_cmp = true;
+        };
+        workspaces = [
+          {
+            name = "personal";
+            path = "~/Documents/Obsidian-Notes";
+          }
+        ];
+        new_notes_location = "notes_subdir";
+        notes_subdir = "06 - In";
+        attachments = {
+          img_folder = "99 - Misc/Images";
+        };
+      };
+    };
   };
 }
