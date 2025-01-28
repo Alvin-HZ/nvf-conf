7a1f16b8dfd8064086c73f27d5dbf9218dfbe56a
Alvin <153151121+Alvin-HZ@users.noreply.github.com>
2025-01-27 21:08:25 -0500

    add terminal keymap

--- modules/terminal.nix
+++ modules/terminal.nix
@@ -1,16 +1,18 @@
 _: {
-  vim.terminal = {
-    toggleterm = {
-      enable = true;
-      lazygit = {
+  vim = {
+    terminal = {
+      toggleterm = {
         enable = true;
-      };
-      mappings = {
-        open = "<A-d>";
-      };
-      setupOpts = {
-        direction = "float";
+        lazygit = {
+          enable = true;
+        };
+        setupOpts = {
+          direction = "float";
+        };
       };
     };
+    keymaps = [
+      { key = "<A-d>"; mode = [ "" "i" "c" "t" ]; action = "<Cmd>execute v:count . \"ToggleTerm\"<CR>"; desc = "Toggle terminal"; }
+    ];
   };
 }
