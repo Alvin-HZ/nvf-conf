c0aed45211eb4654014995b4ba6d9f46430263aa
Alvin <153151121+Alvin-HZ@users.noreply.github.com>
2025-01-27 21:44:25 -0500

    add stuff

--- modules/ui.nix
+++ modules/ui.nix
@@ -1,5 +1,17 @@
 _: {
   vim.ui = {
     noice.enable = true;
+    colorizer.enable = true;
+    fastaction.enable = true;
+    breadcrumbs = {
+      lualine.winbar = {
+        enable = true;
+        alwaysRender = true;
+      };
+      navbuddy = {
+        enable = true;
+      };
+      enable = true;
+    };
   };
 }
