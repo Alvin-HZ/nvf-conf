8d5946bd32d4d1f78b0a3f1fa363b51f747f3ba1
Alvin <153151121+Alvin-HZ@users.noreply.github.com>
2025-05-28 14:55:45 -0400

    add seperate obsidian version

--- flake.nix
+++ flake.nix
@@ -2,26 +2,29 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nvf.url = "github:notashelf/nvf";
-
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , ...
-    } @ inputs:
-    let
-      system = "x86_64-linux";
-      neovimConfigured = inputs.nvf.lib.neovimConfiguration {
-        inherit (nixpkgs.legacyPackages.${system}) pkgs;
-        modules = [
-          ./modules
-        ];
-      };
-    in
-    {
-      packages.${system} = {
-        default = neovimConfigured.neovim;
-      };
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    neovimConfigured = inputs.nvf.lib.neovimConfiguration {
+      inherit (nixpkgs.legacyPackages.${system}) pkgs;
+      modules = [
+        ./modules
+      ];
     };
+    obsidian = inputs.nvf.lib.neovimConfiguration {
+      inherit (nixpkgs.legacyPackages.${system}) pkgs;
+      modules = [
+      ];
+    };
+  in {
+    packages.${system} = {
+      default = neovimConfigured.neovim;
+      obsidian = obsidian.neovim;
+    };
+  };
 }
