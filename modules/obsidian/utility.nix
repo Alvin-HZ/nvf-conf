ce3bd774937b915139a40bd2e459f1e281494f82
Alvin <153151121+Alvin-HZ@users.noreply.github.com>
2025-06-01 14:51:55 -0400

    add aerial keybind

--- modules/obsidian/utility.nix
+++ modules/obsidian/utility.nix
@@ -1,19 +1,29 @@
 _: {
-  vim.utility = {
-    surround.enable = true;
-    diffview-nvim.enable = true;
-    outline.aerial-nvim = {
-      enable = true;
-    };
-    images.image-nvim = {
-      enable = true;
-      setupOpts = {
-        editorOnlyRenderWhenFocused = true;
-        integrations.markdown = {
-          enable = true;
-          clearInInsertMode = true;
+  vim = {
+    utility = {
+      surround.enable = true;
+      diffview-nvim.enable = true;
+      outline.aerial-nvim = {
+        enable = true;
+      };
+      images.image-nvim = {
+        enable = true;
+        setupOpts = {
+          editorOnlyRenderWhenFocused = true;
+          integrations.markdown = {
+            enable = true;
+            clearInInsertMode = true;
+          };
         };
       };
     };
+    keymaps = [
+      {
+        key = "<leader>o";
+        mode = ["" "i" "t" "n"];
+        action = "<CMD>AerialToggle!<CR>";
+        desc = "Toggle Outline";
+      }
+    ];
   };
 }
