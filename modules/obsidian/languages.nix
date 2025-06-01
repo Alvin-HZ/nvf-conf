298c9e0a98a396ba0b8e49732fbc3cc1dc40aba4
Alvin <153151121+Alvin-HZ@users.noreply.github.com>
2025-06-01 14:52:36 -0400

    add extra markdown rendering config

--- modules/obsidian/languages.nix
+++ modules/obsidian/languages.nix
@@ -8,7 +8,28 @@ _: {
       markdown = {
         enable = true;
         format.enable = false;
-        extensions.render-markdown-nvim.enable = true;
+        extensions.render-markdown-nvim = {
+          enable = true;
+          setupOpts = {
+            heading = {
+              sign = false;
+              position = "inline";
+              icons = ["\# " "\#\# " "\#\#\# " "\#\#\#\# " "\#\#\#\#\# " "\#\#\#\#\#\# "];
+              width = "block";
+              left_pad = 2;
+              right_pad = 4;
+            };
+            code = {
+              sign = false;
+              left_pad = 2;
+              right_pad = 4;
+              border = "thick";
+            };
+            bullet = {
+              right_pad = 2;
+            };
+          };
+        };
       };
     };
     lsp = {
